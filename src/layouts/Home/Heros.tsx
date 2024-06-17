876a715a60ec86e01b96302059e2c96e018b4fd2
Meeraj <atifmeeraj62@gmail.com>
2024-06-17 08:45:02 +0545

    Heros section updated

--- src/layouts/Home/Heros.tsx
+++ src/layouts/Home/Heros.tsx
@@ -1,44 +1,72 @@
-import React from 'react';
-
 export const Heros = () => {
     return (
         <div>
-            <div className="d-none d-lg-block">
-                <div className="row g-0 mt-5">
-                    {/* First Hero Section */}
-                    <div className="col-md-6 d-flex align-items-center" style={{ backgroundImage: 'url(/Images/PublicImages/image-1.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '400px' }}>
-                        <div></div>
+            <div className='d-none d-lg-block'>
+                <div className='row g-0 mt-5'>
+                    <div className='col-sm-6 col-md-6'>
+                        <div className='col-image-left' style={{backgroundImage: 'url(/Images/PublicImages/image-1.jpg)', backgroundSize: 'cover', height: '100%'}}></div>
                     </div>
-                    <div className="col-md-6 container d-flex justify-content-center align-items-center">
-                        <div className="ml-2">
+                    <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
+                        <div className='ml-2'>
                             <h1>What have you been reading?</h1>
-                            <p className="lead">
+                            <p className='lead'>
                                 The library team would love to know what you have been reading.
                                 Whether it is to learn a new skill or grow within one,
-                                we will be able to provide the top content for you.
+                                we will be able to provide the top content for you!
                             </p>
-                            <a className="btn main-color btn-lg text-white" href="#">Sign Up</a>
+                            <a href='/login' className='btn main-color btn-lg text-white'>Sign up</a>
                         </div>
                     </div>
                 </div>
-
-                <div className="row g-0 mt-5">
-                    {/* Second Hero Section */}
-                    <div className="col-md-6 order-md-2 d-flex align-items-center" style={{ backgroundImage: 'url(/Images/PublicImages/image-2.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '400px' }}>
-                        <div></div>
-                    </div>
-                    <div className="col-md-6 container d-flex justify-content-center align-items-center order-md-1">
-                        <div className="ml-2">
-                            <h1>Discover New Books</h1>
-                            <p className="lead">
-                                Explore a wide variety of new books and authors that we have added
-                                to our collection. Expand your horizons with our recommendations.
+                <div className='row g-0'>
+                    <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
+                        <div className='ml-2'>
+                            <h1>Our collection is always changing!</h1>
+                            <p className='lead'>
+                                Try to check in daily as our collection is always changing! 
+                                We work nonstop to provide the most accurate book selection possible 
+                                for our Luv 2 Read students! We are diligent about our book selection 
+                                and our books are always going to be our 
+                                top priority.
                             </p>
-                            <a className="btn main-color btn-lg text-white" href="#">Explore Now</a>
+                        </div>
+                    </div>
+                    <div className='col-sm-6 col-md-6'>
+                        <div className='col-image-right' style={{backgroundImage: 'url(/Images/PublicImages/image-1.jpg)', backgroundSize: 'cover', height: '100%'}}></div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Mobile Heros */}
+            <div className='d-lg-none'>
+                <div className='container'>
+                    <div className='m-2'>
+                        <div className='col-image-left' style={{backgroundImage: 'url(/Images/PublicImages/image-1.jpg)', backgroundSize: 'cover', height: '250px'}}></div>
+                        <div className='mt-2'>
+                            <h1>What have you been reading?</h1>
+                            <p className='lead'>
+                                The library team would love to know what you have been reading.
+                                Whether it is to learn a new skill or grow within one,
+                                we will be able to provide the top content for you!
+                            </p>
+                            <a href='/login' className='btn main-color btn-lg text-white'>Sign up</a>
+                        </div>
+                    </div>
+                    <div className='m-2'>
+                        <div className='col-image-right' style={{backgroundImage: 'url(/Images/PublicImages/image-1.jpg)', backgroundSize: 'cover', height: '250px'}}></div>
+                        <div className='mt-2'>
+                            <h1>Our collection is always changing!</h1>
+                            <p className='lead'>
+                                Try to check in daily as our collection is always changing! 
+                                We work nonstop to provide the most accurate book selection possible 
+                                for our Luv 2 Read students! We are diligent about our book selection 
+                                and our books are always going to be our 
+                                top priority.
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
