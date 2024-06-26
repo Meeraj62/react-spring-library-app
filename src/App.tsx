c396af1d204f7d32bceae22fa0f1c146e9b4de56
Meeraj <atifmeeraj62@gmail.com>
2024-06-26 12:05:02 +0545

    SearchTheBook component completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Home } from './layouts/Home/Home';
+import { SearchBooks } from './layouts/SearchBooks/SearchBooks';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+
+      <SearchBooks />
       <Footer />
     </div>
   );
--- src/layouts/SearchBooks/SearchBooks.tsx
+++ src/layouts/SearchBooks/SearchBooks.tsx
@@ -1,46 +1,125 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { SearchTheBook } from "./components/SearchTheBook";
 
 export const SearchBooks = () => {
-    
     const [books, setBooks] = useState<BookModel[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [httpError, setHttpError] = useState(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [httpError, setHttpError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchBooks = async () => {
-            const baseUrl: string = 'http://localhost:8080/api/books';
-            
-            const url: string = `${baseUrl}/page=0&size=5`;
+            try {
+                const baseUrl: string = 'http://localhost:8080/api/books';
+                const url: string = `${baseUrl}?page=0&size=5`;
+                const response = await fetch(url);
 
-            const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('Something went wrong!');
+                }
 
-            if(!response.ok) {
-                throw new Error('Something went wrong!!!');
+                const responseJson = await response.json();
+                const responseData = responseJson._embedded.books;
+
+                const loadedBooks: BookModel[] = responseData.map((book: any) => ({
+                    id: book.id,
+                    title: book.title,
+                    description: book.description,
+                    author: book.author,
+                    copies: book.copies,
+                    copiesAvailable: book.copiesAvailable,
+                    category: book.category,
+                    img: book.img
+                }));
+
+                setBooks(loadedBooks);
+            } catch (error: any) {
+                setHttpError(error.message);
+            } finally {
+                setIsLoading(false);
             }
+        };
 
-            const responseJson = await response.json();
-        
-            const responseData = responseJson._embedded.books;
-
-            const loadedBooks: BookModel[] = [];
-
-            for (const key in responseData) {
-                loadedBooks.push({
-                    id: responseData[key].id,
-                    title: responseData[key].title,
-                    description: responseData[key].description,
-                    author: responseData[key].author,
-                    copies: responseData[key].copies,
-                    copiesAvailable: responseData[key].copiesAvailable,
-                    category: responseData[key].category,
-                    img: responseData[key].img
-                });
-            }
-        }
+        fetchBooks();
     }, []);
 
-    return(
+    if (isLoading) {
+        return <SpinnerLoading />;
+    }
 
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="container">
+            <SearchHeader />
+            <ResultsInfo resultsCount={22} />
+            <BooksList books={books} />
+        </div>
     );
-}
+};
+
+const SearchHeader = () => (
+    <div className="row mt-5">
+        <div className="col-6">
+            <div className="d-flex">
+                <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-labelledby="Search"
+                />
+                <button className="btn btn-outline-success">Search</button>
+            </div>
+        </div>
+        <div className="col-4">
+            <CategoryDropdown />
+        </div>
+    </div>
+);
+
+const CategoryDropdown = () => (
+    <div className="dropdown">
+        <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+            Category
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            {['All', 'Front End', 'Back End', 'Dev Ops'].map((category) => (
+                <li key={category}>
+                    <a className="dropdown-item" href="#">
+                        {category}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+
+const ResultsInfo = ({ resultsCount }: { resultsCount: number }) => (
+    <>
+        <div className="mt-3">
+            <h5>Number of results: ({resultsCount})</h5>
+        </div>
+        <p>1 to 5 of {resultsCount} items:</p>
+    </>
+);
+
+const BooksList = ({ books }: { books: BookModel[] }) => (
+    <div>
+        {books.map((book) => (
+            <SearchTheBook book={book} key={book.id} />
+        ))}
+    </div>
+);
